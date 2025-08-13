cad8886b244ed6a7fe8ca8a905ffeb2d9b96badc
Carlosyoot <carlosdanielyoot@gmail.com>
2025-08-13 16:57:58 -0300

    refactor(oracle): migrar Client/OracleClient.js para TypeScript

---
+++ src/Client/OracleCliente.ts
@@ -0,0 +1,12 @@
+import oracledb from 'oracledb';
+import 'dotenv/config';
+
+oracledb.initOracleClient();
+
+const pool = await oracledb.createPool({
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASS!,
+  connectString: process.env.DB_CONNECT!
+});
+
+export default pool;
